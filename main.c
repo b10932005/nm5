09783a63b9b9c590686b7c4a8095a5ffa1b65fe1
8LWXpg <v3ak6xhthk@gmail.com>
2022-11-03 15:02:56 +0800

    fixed

--- main.c
+++ main.c
@@ -8,11 +8,7 @@ int main() {
 	complex_mat *m2 = cmat_new(2, 1, b);
 	complex_mat *m3 = cmat_mul(m1, m2);
 	cmat_printf("%6.2lf%+6.2lfi", 3, m1, m2, m3);
-	complex_mat *m4 = cmat_col_replace(m1, m2, 0);
-	complex_f64 d1 = cmat_det(m4);
-	complex_f64 d2 = cmat_det(m1);
-	cf64_printf("%6.2lf%+6.2lfi\n", 3, d1, d2, d1 / d2);
 	complex_f64 x[2] = {cmat_det(cmat_col_replace(m1, m2, 0)) / cmat_det(m1),
 						cmat_det(cmat_col_replace(m1, m2, 1)) / cmat_det(m1)};
-	cf64_printf("%6.2lf%+6.2lfi\n", 3, x[0], x[1], cmat_det(m4) / cmat_det(m1));
+	cf64_printf("%6.2lf%+6.2lfi\n", 2, x[0], x[1]);
 };
